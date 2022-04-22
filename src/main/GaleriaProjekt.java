c3863d9d059d6cbecd4d5aa61eb2696891ff3daf
HeczKlaudia <kicsihold21@gmail.com>
2022-04-22 15:33:11 +0200

    példányosítás, kep.txt fájl

--- src/main/GaleriaProjekt.java
+++ src/main/GaleriaProjekt.java
@@ -1,16 +1,29 @@
 package main;
 
-/**
- *
- * @author Hecz.K.Noemi
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import modell.Galeria;
+import modell.KiallitasiTargy;
+
 public class GaleriaProjekt {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        
+        Galeria g1 = new Galeria("cím1", "Antal István");
+        Galeria g2 = new Galeria("cím1", "Antal István");
+        
+        List<String> kep = Files.readAllLines(Paths.get("../res/kep.txt"));
+        
+        List<KiallitasiTargy> ktargyak = new ArrayList<>();
+        Collections.sort(ktargyak);
+        for (KiallitasiTargy targy : ktargyak) {
+            System.out.println(targy);
+        }
+        
     }
     
 }
