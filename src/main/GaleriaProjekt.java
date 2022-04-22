63f87dea78e170b3543375a3d64b71bbd1e24d95
HeczKlaudia <kicsihold21@gmail.com>
2022-04-22 14:43:16 +0200

    alap mappák és classok létrehozása

---
+++ src/main/GaleriaProjekt.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author Hecz.K.Noemi
+ */
+public class GaleriaProjekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
