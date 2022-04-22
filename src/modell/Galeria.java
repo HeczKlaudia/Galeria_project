1c585e07b062386d5b12a8ad0abd6d9eae005e08
HeczKlaudia <kicsihold21@gmail.com>
2022-04-22 15:15:53 +0200

    metódusok létrehozása, rendezés

--- src/modell/Galeria.java
+++ src/modell/Galeria.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modell;
 
-/**
- *
- * @author Hecz.K.Noemi
- */
-public class Galeria {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class Galeria extends KiallitasiTargy{
     
+    private ArrayList<KiallitasiTargy> kTargy;
+
+    public Galeria(String cim, String keszito) {
+        super(cim, keszito);
+    }
+    
+    public void felvesz(KiallitasiTargy targy) {
+        kTargy.add(targy);
+    }
+    
+    public void kepMegjelenit() {
+        try (ObjectOutputStream ki = new ObjectOutputStream(new FileOutputStream("kep.png"))) {
+            ki.writeObject(this);
+            System.out.println("megjelenítés folyamatban...");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("nem lehet megjeleníteni...");
+        }
+    }
+    
+    public void jovobeliDatum() {
+        
+    }
+
 }
